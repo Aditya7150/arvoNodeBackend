a8bae9af464c0d9e428402a5df31facebfd504ce
Aditya <Developer@apples-MacBook-Pro-2.local>
2023-10-27 11:59:23 +0530

    first commit

---
+++ function/api.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const serverless= require('serverless-http')
+const morgan = require('morgan');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const app = express();
+
+// connect to db
+mongoose
+    .connect(process.env.DATABASE, {
+        useNewUrlParser: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true,
+        useCreateIndex: true
+    })
+    .then(() => console.log('DB connected'))
+    .catch(err => console.log('DB CONNECTION ERROR: ', err));
+
+// import routes
+const authRoutes = require('../routes/auth');
+const userRoutes = require('../routes/user');
+
+// app middlewares
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+// app.use(cors()); // allows all origins
+if ((process.env.NODE_ENV = 'development')) {
+    app.use(cors({ origin: `http://localhost:3000` }));
+}
+app.get('/',(req,res)=>{
+    res.json({
+        "hello":"hi"
+    })
+})
+// middleware
+app.use('/.netlify/functions/api', authRoutes);
+app.use('/.netlify/functions/api', userRoutes);
+
+const port = process.env.PORT || 8000;
+app.listen(port, () => {
+    console.log(`API is running on port ${port}`);
+});
+
+module.exports.handler = serverless(app)
